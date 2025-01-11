bc33f547f4caf6e2a2e48b6b86022b601f3542f8
simeiro <shitimi1633@gmail.com>
2025-01-11 14:14:17 +0900

    bpm_histgram.pyの追加

---
+++ bpm_histgram.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '1.0.0'
+__date__ = '2025/01/10'
+
+import requests
+import matplotlib.pyplot as plt
+
+"""
+SongleAPIのBPM情報からヒストグラムを表示するプログラム
+"""
+
+URL = "https://widget.songle.jp/api/v1/song/beat.json?url=http://www.nicovideo.jp/watch/sm44194393"
+
+def main():
+    try:
+        response = requests.get(URL)
+        if response.status_code == 404:
+            exit()
+    except Exception as e:
+        print(e)
+        exit()
+    beats = response.json()['beats']
+    bpms = []
+    for beat in beats:
+        bpms.append(beat['bpm'])
+    
+    plt.hist(bpms, bins='auto')
+    plt.xlabel('BPM')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Data')
+    plt.show()
+    
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
